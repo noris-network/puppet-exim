a2609ff05342f307b372669b0b55538661b044ee
Manuel Leiner <manuel.leiner@noris.de>
2020-05-25 12:44:58 +0200

    Implement addresslist analog to hostlist, issue #12

---
+++ manifests/addresslist.pp
@@ -0,0 +1,22 @@
+# == Define: exim::addresslist
+#
+# This define can be used to create addresslists, which can be referenced later
+#   http://www.exim.org/exim-html-current/doc/html/spec_html/ch-domain_host_address_and_local_part_lists.html
+#
+# === Parameters
+#
+# [*addresses*]
+#   Array containing a list of hosts
+#
+
+define exim::addresslist (
+  $addresses=undef,
+  ){
+  $list     = $addresses
+  $listtype = 'addresslist'
+  concat::fragment { "addresslist-${title}":
+    target  => $::exim::config_path,
+    content => template("${module_name}/list.erb"),
+    order   => '0003',
+  }
+}
