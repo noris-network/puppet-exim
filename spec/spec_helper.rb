a26ebd81ff2003dd39eeba0bcd7cc4ba51f9c03e
Manuel Leiner <manuel.leiner@noris.de>
2020-09-16 12:05:45 +0200

    ISSUE-49: Get rid of FacterDB facter version incompats (#50)
    * ISSUE-49: Get rid of FacterDB facter version incompats
    
    * ISSUE-49: Get rid of FacterDB facter version incompats

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -38,6 +38,7 @@ end
 
 RSpec.configure do |c|
   c.default_facts = default_facts
+  c.default_facter_version = '3.6.9'
   c.before :each do
     # set to strictest setting for testing
     # by default Puppet runs at warning level
